b84a3580c2271cb6628dec690ba96e48c99d7cb9
mohamedshiras <mchiya1003@gmail.com>
2026-03-06 19:06:43 +0530

    added chats and profile

--- app/src/main/java/com/example/apartmentmanagementsystem/FeedActivity.java
+++ app/src/main/java/com/example/apartmentmanagementsystem/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.example.apartmentmanagementsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,18 +9,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_feed);
         this.getSupportActionBar().hide();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavBar);
+        bottomNav.setSelectedItemId(R.id.nav_feed);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_feed) {
+                // Already here, do nothing
+                return true;
+
+            } else if (id == R.id.nav_announcements) {
+                startActivity(new Intent(this, AnnouncementActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+
+            } else if (id == R.id.nav_chat) {
+                startActivity(new Intent(this, ChatActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+
+            } else if (id == R.id.nav_services) {
+                startActivity(new Intent(this, ServicesActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+
+            } else if (id == R.id.nav_profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            }
+
+            return false;
         });
     }
 }
